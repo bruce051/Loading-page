a9bb1cd58a497bb8f116f2165bc8741e187208b0
bruce051 <adambruce051@gmail.com>
2022-11-30 00:24:21 +0500

    some commit

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+let kk = document.querySelector('.kk');
+// let count = 0;
+// let int = setInterval(()=>{
+//     console.log(count += 1);
+//     if (count == 3) {
+//         clearInterval(int)
+//     }
+// }, 1000);
+setTimeout(() => {
+    kk.style.display = 'none'
+}, 3000);
